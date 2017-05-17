af25d7fbb0f1edf58b2faf3e1d94e73ff73c2bcd
[username] <[email]>
2017-05-18 02:19:44 +0800

    modify discount

--- views/components/dineshop/discount.vue
+++ views/components/dineshop/discount.vue
@@ -17,8 +17,8 @@
             <el-table :data="shopdata" style="width: 100%" class="discount-table">
               <el-table-column prop="timeslot" label="时间段\日期"></el-table-column>
               <el-table-column v-for="(item, index) in this.datelist" :label="item.date">
-                <template scope="discount">
-                  <el-button size="small" @click.stop="toDetailDiscount(discount.row,index)">{{discount.row.discountdata[index].discount.length?'查看':'新增'}}</el-button>
+                <template scope="scope">
+                  <el-button size="small" @click.stop="toDetailDiscount(scope.row,index)">{{scope.row.discountdata[index].discid?'查看':'新增'}}</el-button>
                 </template>
               </el-table-column>
             </el-table>
@@ -32,7 +32,7 @@
         title="编辑时间段"
         :visible.sync="editordialogvisible"
         size="tiny"
-        :before-close="handleClose">
+        :close-on-click-modal="false">
       <div style="max-height: 300px;overflow: auto">
         <el-row class="editor-dialog-row" v-for="(item,index) in timesoltlist">
           <span>{{item.timeslot}}</span>
@@ -49,37 +49,43 @@
         title="折扣信息"
         :visible.sync="discountdialogvisible"
         size="tiny"
-        :before-close="handleClose">
+        :close-on-click-modal="false">
       <el-row class="discount-dialog-row">
-        <span>2017-05-12&nbsp;&nbsp;&nbsp;&nbsp;10:00-11:00</span>
-        <span class="text-button" style="float: right;" @click.stop="adddialogvisible=true">编辑</span>
+        <span>{{currentdate}}&nbsp;&nbsp;&nbsp;&nbsp;{{currenttimeslot.timeslot}}</span>
+        <span class="text-button" style="float: right;" @click.stop="discountdialogvisible=false,modifydialogvisible=true">编辑</span>
       </el-row>
-      <el-table :data="shopdata" :border="true">
-        <el-table-column prop="name" label="菜品名称" align="center"></el-table-column>
-        <el-table-column prop="discount" label="折扣" align="center"></el-table-column>
+      <el-table :data="currentdiscountdishlist.discountdishlist">
+        <el-table-column prop="dishname" label="菜品名称" align="center"></el-table-column>
+        <el-table-column :formatter="formatterDiscount" label="折扣" align="center"></el-table-column>
       </el-table>
     </el-dialog>
     <el-dialog
         class="discount-dialog"
-        title="折扣信息"
-        :visible.sync="adddialogvisible"
+        title="编辑折扣信息"
+        :visible.sync="modifydialogvisible"
         size="tiny"
-        :before-close="handleClose">
+        :close-on-click-modal="false">
       <el-row class="discount-dialog-row">
-        <span>2017-05-12&nbsp;&nbsp;&nbsp;&nbsp;10:00-11:00</span>
-        <span class="text-button" style="float: right;margin-left: 20px;" @click.stop="adddialogvisible = false">完成</span>
+        <span>{{currentdate}}&nbsp;&nbsp;&nbsp;&nbsp;{{currenttimeslot.timeslot}}</span>
+        <span class="text-button" style="float: right;margin-left: 20px;" @click.stop="modifyDiscountList()">完成</span>
         <span class="text-button" style="float: right;" @click.stop="inputdialogvisible=true">新增</span>
       </el-row>
-      <el-table :data="shopdata" border="true">
-        <el-table-column prop="name" label="菜品名称" align="center"></el-table-column>
-        <el-table-column prop="discount" label="折扣" align="center"></el-table-column>
+      <el-table :data="currentdiscountdishlist.discountdishlist">
+        <el-table-column prop="dishname" label="菜品名称" align="center"></el-table-column>
+        <el-table-column :formatter="formatterDiscount" label="折扣" align="center"></el-table-column>
+        <el-table-column align="center">
+          <template><el-icon name="circle-cross"></el-icon></template>
+        </el-table-column>
       </el-table>
     </el-dialog>
     <el-dialog
         title="新增折扣信息"
         :visible.sync="inputdialogvisible"
         size="tiny"
-        :before-close="handleClose">
+        :close-on-click-modal="false">
+      <el-row class="discount-dialog-row">
+        <span style="margin-bottom: 10px;">{{currentdate}}&nbsp;&nbsp;&nbsp;&nbsp;{{currenttimeslot.timeslot}}</span>
+      </el-row>
       <el-form ref="adddiscountdata" :model="adddiscountdata" label-width="80px" label-position="right">
         <el-form-item label="菜品名称:">
           <el-input v-model="adddiscountdata.name" placeholder="请输入菜品名称"></el-input>
@@ -90,14 +96,14 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click.stop="inputdialogvisible = false">取 消</el-button>
-        <el-button type="primary" @click.stop="inputdialogvisible = false">确 定</el-button>
+        <el-button type="primary" @click.stop="addDiscountDish()">确 定</el-button>
       </span>
     </el-dialog>
     <el-dialog
         title="新增时间段"
         :visible.sync="addtimeslotdialogvisible"
         size="tiny"
-        :before-close="handleClose">
+        :close-on-click-modal="false">
       <el-row>
         新增时间段:
         <el-time-select
@@ -150,7 +156,7 @@ export default {
       datelist: [],
       editordialogvisible: false,
       discountdialogvisible: false,
-      adddialogvisible: false,
+      modifydialogvisible: false,
       inputdialogvisible: false,
       addtimeslotdialogvisible: false,
       addtimeslot:{
@@ -162,19 +168,21 @@ export default {
         discount:''
       },
       timesoltlist: [],
-      discountdishlist:[],
+      currentdiscountdishlist:{},
+      currentdate:'',
+      currenttimeslot:{}
     }
   },
   methods: {
-    handleClick(tab, event) {
-      console.log(tab, event);
-    },
-    confirmTimeSlot() {
-      this.editordialogvisible = false;
-      this.shopdata = [];
+    getDineshopDiscount(){
       ajax.get('/admin/shop/getDineshopDiscount', {params:{ shopid:this.shopinfo.shopid}}).then((response) => {
         if (response.data && response.data.code > 0) {
           this.shopdata = response.data.list;
+          for(let i=0;this.shopdata[0].discountdata[i];i++){
+            this.datelist.push({
+              date:this.shopdata[0].discountdata[i].date
+            });
+          };
         } else {
           this.$message.error(response.data.msg);
         }
@@ -182,22 +190,35 @@ export default {
         this.$message.error(e.toString());
       });
     },
-    toDetailDiscount(data,index){
-      data.discountdata[index].discount.length ? this.discountdialogvisible=true : this.inputdialogvisible=true
+    handleClick(tab, event) {
+      console.log(tab, event);
     },
-    handleClose(done) {
-      console.log('关闭弹窗');
-      done();
+    confirmTimeSlot() {
+      this.editordialogvisible = false;
+      this.shopdata = [];
+      getDineshopDiscount();
+    },
+    toDetailDiscount(data,index){
+      let discountdata = data.discountdata[index];
+      this.currentdate = discountdata.date;
+      this.currenttimeslot.id = data.id;
+      this.currenttimeslot.timeslot = data.timeslot;
+      if(discountdata.discid){
+        this.currentdiscountdishlist.discid = discountdata.discid;
+        this.currentdiscountdishlist.discountdishlist=discountdata.discount;
+        this.discountdialogvisible=true;
+      }else{
+        this.inputdialogvisible=true;
+      }
     },
     showTimeslot(){
       this.editordialogvisible = true;
       this.timesoltlist = [];
-      this.datelist = [];
       ajax.get('/admin/shop/getDiscountTimeslot', {params:{ indicator:{async: true} }}).then((response) => {
         if (response.data && response.data.code > 0) {
-          for (var i = 0; i < response.data.list.length; i++) {
+          for (let i = 0; i < response.data.list.length; i++) {
             let timeslot = response.data.list[i];
-            this.timesoltlist.push({id: timeslot.id, timeslot: timeslot.startime.slice(0,5)+'-'+timeslot.endtime.slice(0,5)});
+            this.timesoltlist.push({id: timeslot.id, timeslot: timeslot.timeslot});
           }
         } else {
           this.$message.error(response.data.msg);
@@ -207,7 +228,7 @@ export default {
       });
     },
     addTimeSlot(){
-      ajax.get('/admin/shop/addDiscountTimeslot', {params:{ startime:this.addtimeslot.starttime,endtime:this.addtimeslot.endtime}}).then((response) => {
+      ajax.get('/admin/shop/addDiscountTimeslot', {params:{ timeslot:this.addtimeslot.starttime+'-'+this.addtimeslot.endtime}}).then((response) => {
         if (response.data && response.data.code > 0) {
           let slotid = response.data.info.slotid;
           this.timesoltlist.push({id: slotid,timeslot:this.addtimeslot.starttime+'-'+this.addtimeslot.endtime});
@@ -221,11 +242,49 @@ export default {
       });
     },
     deleteTimeSlot(id){
-      ajax.get('/admin/shop/delDiscountTimeslot', {params:{ slotid:id}}).then((response) => {
-        if (response.data && response.data.code > 0) {
-          for (var i = 0; i < this.timesoltlist.length; i++) {
-            if(this.timesoltlist[i].id == id) this.timesoltlist.splice(i,1);
+      let timesolt = this.timesoltlist;
+      this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+        closeOnClickModal: false,
+        callback: function (action,instance) {
+          if(action == 'confirm'){
+            ajax.get('/admin/shop/delDiscountTimeslot', {params:{ slotid:id}}).then((response) => {
+              if (response.data && response.data.code > 0) {
+                for (let i = 0; i < timesolt.length; i++) {
+                  if(timesolt[i].id == id){timesolt.splice(i,1)};
+                }
+              } else {
+                this.$message.error(response.data.msg);
+              }
+            }).catch((e) => {
+              this.$message.error(e.toString());
+            });
           }
+        }
+      });
+    },
+    formatterDiscount(row,column){
+      return row.num * 10;
+    },
+    addDiscountDish(){
+      ajax.get('/admin/shop/addDineshopDiscount', {params:{ shopid:this.shopinfo.shopid,date:this.currentdate,timeslot:this.currenttimeslot.id,discount:this.adddiscountdata.discount+'|1'}}).then((response) => {
+        if (response.data && response.data.code > 0) {
+          this.inputdialogvisible = false;
+          getDineshopDiscount();
+        } else {
+          this.$message.error(response.data.msg);
+        }
+      }).catch((e) => {
+        this.$message.error(e.toString());
+      });
+    },
+    modifyDiscountList(){
+      ajax.get('/admin/shop/modDineshopDiscount', {params:{id:this.currentdiscountdishlist.discid,discount:''}}).then((response) => {
+        if (response.data && response.data.code > 0) {
+          this.modifydialogvisible = false;
+          getDineshopDiscount();
         } else {
           this.$message.error(response.data.msg);
         }
